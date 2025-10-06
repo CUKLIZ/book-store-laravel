57eea15bcf07ae9c9714af0653f50f40c18f99b1
CUKLIZ <wiratamarava41@gmail.com>
2025-10-06 16:18:15 +0700

    Fix Seeders

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -12,59 +12,34 @@ class CategorySeeder extends Seeder
      */
     public function run(): void
     {
-        $categories = [
+        $categoryNames = [
             // Fiksi
-            'Novel',
-            'Cerpen',
-            'Fantasi',
-            'Petualangan',
-            'Misteri / Thriller',
-            'Horor',
-            'Romantis',
-            'Fiksi Ilmiah (Sci-Fi)',
-            'Drama',
-            'Fiksi Sejarah',
+            'Novel', 'Cerpen', 'Fantasi', 'Petualangan', 'Misteri / Thriller',
+            'Horor', 'Romantis', 'Fiksi Ilmiah (Sci-Fi)', 'Drama', 'Fiksi Sejarah',
 
             // Non-Fiksi
-            'Biografi & Autobiografi',
-            'Motivasi & Pengembangan Diri',
-            'Sejarah',
-            'Politik & Sosial',
-            'Agama & Spiritual',
-            'Sains & Teknologi',
-            'Psikologi',
-            'Pendidikan',
-            'Kesehatan & Gaya Hidup',
-            'Ekonomi & Bisnis',
+            'Biografi & Autobiografi', 'Motivasi & Pengembangan Diri', 'Sejarah',
+            'Politik & Sosial', 'Agama & Spiritual', 'Sains & Teknologi',
+            'Psikologi', 'Pendidikan', 'Kesehatan & Gaya Hidup', 'Ekonomi & Bisnis',
 
             // Anak & Remaja
-            'Buku Cerita Anak',
-            'Buku Bergambar',
-            'Buku Edukasi Anak',
-            'Komik / Manga',
-            'Teenlit / Young Adult',
+            'Buku Cerita Anak', 'Buku Bergambar', 'Buku Edukasi Anak',
+            'Komik / Manga', 'Teenlit / Young Adult',
 
             // Akademik & Referensi
-            'Buku Pelajaran Sekolah',
-            'Buku Kuliah / Akademik',
-            'Kamus & Ensiklopedia',
-            'Buku Panduan / Manual',
-            'Jurnal & Penelitian',
+            'Buku Pelajaran Sekolah', 'Buku Kuliah / Akademik', 'Kamus & Ensiklopedia',
+            'Buku Panduan / Manual', 'Jurnal & Penelitian',
 
             // Lain-lain
-            'Buku Masak',
-            'Hobi & Kerajinan',
-            'Seni & Desain',
-            'Buku Perjalanan (Travel)',
-            'Puisi & Sastra',
-            'Majalah',
-            'Buku Keagamaan',
+            'Buku Masak', 'Hobi & Kerajinan', 'Seni & Desain',
+            'Buku Perjalanan (Travel)', 'Puisi & Sastra', 'Majalah', 'Buku Keagamaan',
         ];
-        foreach ($categories as $name => $slug) {
+
+        foreach ($categoryNames as $name) {
             Category::create([
                 'name' => $name,
-                'slug' => $slug,
+                'slug' => \Illuminate\Support\Str::slug($name),
             ]);
-        } 
+        }
     }
 }
